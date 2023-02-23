82f4cd8c9ac609129815352378e1edbed357ce41
antgonrui <antgonrui@alu.edu.gva.es>
2023-02-23 11:33:13 +0100

    Initial commit

---
+++ src/test/java/com/example/GymRoutine/GymRoutineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GymRoutine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymRoutineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
